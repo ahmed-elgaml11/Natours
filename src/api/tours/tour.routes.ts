f13b94a6c97bc5495f143457de2c2be1c80698fe
ahmed elgaml <a7medelgaml11a@gmail.com>
2025-04-14 23:21:16 +0200

    add a tour model and import data from json file to the db documents

--- src/api/tours/tour.routes.ts
+++ src/api/tours/tour.routes.ts
@@ -1,4 +1,18 @@
 import express from "express";
 const router = express.Router();
+import tourControllers from './tour.controller'
+
+
+router
+    .route('/')
+    .get(tourControllers.getAllTours)
+    .post(tourControllers.addTour)
+
+router 
+    .route('/:id')
+    .get(tourControllers.getTour)
+    .patch(tourControllers.updateTour)
+    .delete(tourControllers.deleteTour)
+
 
 export default router

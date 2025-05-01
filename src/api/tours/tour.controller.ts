98225c403912184250c5e6af5faa61f55608824f
ahmed elgaml <a7medelgaml11a@gmail.com>
2025-05-01 15:26:58 +0300

    Add advanced query handler for Tour API with filtering, sorting, field limiting, and pagination

--- src/api/tours/tour.controller.ts
+++ src/api/tours/tour.controller.ts
@@ -1,13 +1,60 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, json } from 'express';
 import { catchAsync } from '../../utils/catchAsync';
 import { toursResponse } from '../../types/toursResponse';
 import * as Servises from './tour.services'
 import { AppError } from '../../utils/appError';
-import { ITour } from './tour.model'
+import { ITour, Tour } from './tour.model'
 import { updatedTourType } from './tour.schema'
 
 export const getAllTours = catchAsync(async(req: Request, res: Response<toursResponse>, next: NextFunction) => {
-    const tours = await Servises.getAllTours();
+    // filtering
+    const queryObj = {...req.query}
+    const excludedFields = ['sort', 'page', 'limit', 'fields']
+    excludedFields.forEach((ele) => delete queryObj[ele]) 
+
+    //advanced filtering         ?age[gte]=10
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+    let query = Tour.find(JSON.parse(queryStr))
+
+    //sort ('name age')                              ?sort=duration,name
+    if(req.query.sort && typeof req.query.sort ==='string' ){
+        const sortedBy = req.query.sort.split(',').join(' ')
+        query = query.sort(sortedBy)
+    }else{
+        query = query.sort('-createdAt')
+    }
+
+    // fields limiting: select(-name age)              ?fields=duration,name
+    if(req.query.fields && typeof req.query.fields ==='string'){
+        const fields = req.query.fields.split(',').join(' ')
+        query = query.select(fields)
+    }else{
+        query = query.select('-__v')
+    }
+
+
+
+    //pagination               ?page=3&limit=3
+    const page: number = Number(req.query.page) || 1;
+    const limit: number = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit
+
+    query.skip(skip).limit(limit)
+
+    if(req.query.page){
+        const tourNums = await Tour.countDocuments()
+        if(skip >= tourNums)  throw new AppError('this page dosnt exist', 404);
+        
+    }
+
+    // execute Query
+    const tours = await query
+
+
+
+    // const tours = await Servises.getAllTours();
 
     res.status(200).json({
         status: 'success',

be0c733efece5ed041210ebacc8c4370f0a51e79
ahmed elgaml <a7medelgaml11a@gmail.com>
2025-05-14 20:30:08 +0300

    chore: add security and logging middlewares (helmet, morgan) and limit request body size

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,24 @@ import firstResponse from './types/firstResponse';
 import api from './api'
 import errorHandler from './middlewares/errorHandler';
 import { rateLimit } from 'express-rate-limit'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
 
 const app = express();
-app.use(express.json());
+app.use(helmet());
+app.use(express.json({ limit: '20kb' }));
+if (process.env.NODE_ENV === 'development')   app.use(morgan('dev'))
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
     message: 'too many requests from this IP, try again in 15 minutes'
 })
-
 app.use('/api', limiter)
+
+
+
+
 
 app.get<{}, firstResponse>('/', (req, res) => {
     res.status(200).json({

478bf1cc6f10f5509aa09d3926bae808aeae8c46
ahmed elgaml <a7medelgaml11a@gmail.com>
2025-05-14 19:44:59 +0300

    feat(security): add rate limiting middleware to prevent abuse

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path'
-dotenv.config({path: path.join(__dirname, '../config.env')})
-import { AppError } from './utils/appError'; 
+dotenv.config({ path: path.join(__dirname, '../config.env') })
+import { AppError } from './utils/appError';
 import firstResponse from './types/firstResponse';
 import api from './api'
 import errorHandler from './middlewares/errorHandler';
-
+import { rateLimit } from 'express-rate-limit'
 
 
 const app = express();
 app.use(express.json());
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message: 'too many requests from this IP, try again in 15 minutes'
+})
 
+app.use('/api', limiter)
 
 app.get<{}, firstResponse>('/', (req, res) => {
     res.status(200).json({

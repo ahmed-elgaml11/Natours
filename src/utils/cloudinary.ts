98abd9eb151e49f8314999edffc7550a458d730c
ahmed elgaml <a7medelgaml11a@gmail.com>
2025-06-10 06:34:28 +0300

    start to upload images on cloudinary

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -4,3 +4,28 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+export const cloudinaryUploadImage = async (filePath: string) => {
+    try{
+
+        const data =  await cloudinary.uploader.upload(filePath, {
+            resource_type: 'auto'
+        })
+        return data;    
+    }
+    catch(err){
+         throw err
+    }
+}
+
+export const cloudinaryRemoveImage = async (imageID: string) => {
+    try{
+        const data =  await cloudinary.uploader.destroy(imageID, {
+            resource_type: 'auto'
+        })
+        return data;    
+    }
+    catch(err){
+        return err
+    }
+}

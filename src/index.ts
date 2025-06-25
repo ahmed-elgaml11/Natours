ddf4b5db56818087e21423d17f91544eb10fc571
ahmed elgaml <a7medelgaml11a@gmail.com>
2025-06-25 14:20:01 +0300

    change the port the app listening to

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ process.on('uncaughtException', (err: Error) => {      // synchrounous code whic
 import app from './app'
 import {main} from './db'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 main()
 .then(() => {
     console.log('connected to db');
